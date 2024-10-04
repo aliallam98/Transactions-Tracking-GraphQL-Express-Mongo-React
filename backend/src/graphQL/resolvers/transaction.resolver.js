f1874aa351571c96e0ad84982b833a5e9dbff22e
aliallam98 <allam98.ali@gmail.com>
2024-10-04 21:03:29 +0300

    Completed transaction Resolvers

--- backend/src/graphQL/resolvers/transaction.resolver.js
+++ backend/src/graphQL/resolvers/transaction.resolver.js
@@ -1,67 +1,66 @@
 import transactionModel from "../../DB/models/Transaction.mode.js";
 
-// const transactionResolver = {
-//   Query: {
-//     // transactions: async (_, _, context) => {
-//     //   const userId = context.getUser()._id;
-//     //   if (!userId) throw new Error("Not Authenticated");
+const transactionResolver = {
+  Query: {
+    transactions: async (_, _, context) => {
+      const userId = context.getUser()._id;
+      if (!userId) throw new Error("Not Authenticated");
 
-//     //   const transactions = await transactionModel.find({ userId });
-//     //   return transactions;
-//     // },
-//     transaction: async (_, { transactionId, context }) => {
-//       const userId = context.getUser()._id;
-//       if (!userId) throw new Error("Not Authenticated");
+      const transactions = await transactionModel.find({ userId });
+      return transactions;
+    },
+    transaction: async (_, { transactionId},context) => {
+      const userId = context.getUser()._id;
+      if (!userId) throw new Error("Not Authenticated");
 
-//       const transaction = await transactionModel.findById(transactionId);
+      const transaction = await transactionModel.findById(transactionId);
 
-//       if (!transaction) throw new Error("Cannot find transaction");
+      if (!transaction) throw new Error("Cannot find transaction");
 
-//       return transaction;
-//     },
-//   },
-//   Mutation: {
-//     createTransaction: async (_, { input }, context) => {
-//       const userId = context.getUser()._id;
-//       if (!userId) throw new Error("Not Authenticated");
+      return transaction;
+    },
+  },
+  Mutation: {
+    createTransaction: async (_, { input }, context) => {
+      const userId = context.getUser()._id;
+      if (!userId) throw new Error("Not Authenticated");
 
-//       const transaction = await transactionModel.create({
-//         ...input,
-//         userId,
-//       });
+      const transaction = await transactionModel.create({
+        ...input,
+        userId,
+      });
 
-//       return transaction;
-//     },
-//     updateTransaction: async (_, { input }, context) => {
-//       const userId = context.getUser()._id;
-//       if (!userId) throw new Error("Not Authenticated");
+      return transaction;
+    },
+    updateTransaction: async (_, { input }, context) => {
+      const userId = context.getUser()._id;
+      if (!userId) throw new Error("Not Authenticated");
 
-//       const transactionToUpdate = await transactionModel.findByIdAndUpdate(
-//         input._id,
-//         {
-//           ...input,
-//         },{
-//           new:true
-//         }
-//       );
+      const transactionToUpdate = await transactionModel.findByIdAndUpdate(
+        input._id,
+        {
+          ...input,
+        },
+        {
+          new: true,
+        }
+      );
 
-//       return transactionToUpdate
-//     },
-//     deleteTransaction: async (_, { transactionId }, context) => {
-//       const userId = context.getUser()._id;
-//       if (!userId) throw new Error("Not Authenticated");
+      return transactionToUpdate;
+    },
+    deleteTransaction: async (_, { transactionId }, context) => {
+      const userId = context.getUser()._id;
+      if (!userId) throw new Error("Not Authenticated");
 
-//       const transactionToDelete = await transactionModel.findByIdAndDelete(
-//         transactionId
-//       );
+      const transactionToDelete = await transactionModel.findByIdAndDelete(
+        transactionId
+      );
 
-//       if (!transactionToDelete) throw new Error("Cannot find transaction");
+      if (!transactionToDelete) throw new Error("Cannot find transaction");
 
-//       return transactionToDelete
-//     },
-//   },
-// };
-
-const transactionResolver = {}
+      return transactionToDelete;
+    },
+  },
+};
 
 export default transactionResolver;
